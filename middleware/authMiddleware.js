a659945a2122027080d49ebd9c0c77cca64f0b5a
hkgandhi123 <hariomgandhi2000@gmail.com>
2025-09-14 21:43:25 -0700

    fix: cleaned up authRoutes duplicates

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,16 +1,18 @@
 import jwt from "jsonwebtoken";
-import User from "../models/User.js";
+import User from "../models/userModel.js";
 
-export const verifyToken = async (req, res, next) => {
+export const protect = async (req, res, next) => {
+  let token = req.cookies.token;
+
+  if (!token) {
+    return res.status(401).json({ message: "Not authorized, no token" });
+  }
+
   try {
-    const token = req.cookies.token;
-    if (!token) return res.status(401).json({ message: "No token provided" });
-
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = await User.findById(decoded.id).select("-password");
     next();
-  } catch (err) {
-    res.status(401).json({ message: "Invalid token" });
+  } catch (error) {
+    return res.status(401).json({ message: "Not authorized, token failed" });
   }
 };
-
