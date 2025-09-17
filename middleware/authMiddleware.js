f1417c6385da602116237dbb70507c231b2362ae
hkgandhi123 <hariomgandhi2000@gmail.com>
2025-09-17 04:46:49 -0700

    Fix: Correct User model import case for Render

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,5 +1,5 @@
 import jwt from "jsonwebtoken";
-import User from "../models/userModel.js";
+import User from "../models/user.js";
 
 // ✅ Protect route middleware
 export const protect = async (req, res, next) => {
